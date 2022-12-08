e43ef16bb2684b962825741d8cdaf2b29ce2a161
kan01234 <safghjkl@gmail.com>
2022-12-08 21:59:15 +0900

    add day6

---
+++ src/main/kotlin/D6P1.kt
@@ -0,0 +1,24 @@
+import java.io.File
+import java.io.InputStream
+
+fun main() {
+    val inputStream: InputStream = File("input/d6-input.txt").inputStream()
+
+    var result = 0
+    val charArray = inputStream.bufferedReader().readLine().toCharArray()
+    var chars = mutableListOf<Char>()
+
+    for (i in charArray.indices) {
+        val c = charArray[i]
+        val index = chars.indexOf(c)
+        if (index >= 0) {
+            chars = chars.subList(index + 1, chars.size)
+        }
+        chars.add(c)
+        if (chars.size == 4) {
+            result = i + 1
+            break
+        }
+    }
+    println(result)
+}
